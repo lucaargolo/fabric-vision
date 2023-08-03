0d7993c1ef3e3c0bd9db1d892bf9291a927c4bc9
lucaargolo <lucaargolo@gmail.com>
2023-08-02 22:15:51 -0300

    oh my god no way way way

--- src/main/kotlin/io/github/lucaargolo/fabricvision/client/ProjectorShader.kt
+++ src/main/kotlin/io/github/lucaargolo/fabricvision/client/ProjectorShader.kt
@@ -31,70 +31,31 @@ object ProjectorShader: PostWorldRenderCallbackV2, ShaderEffectRenderCallback {
         }
     }
 
-    private val mainTransformMatrix = projectorShader.findUniformMat4("MainTransformMatrix")
     private val mainInverseTransformMatrix = projectorShader.findUniformMat4("MainInverseTransformMatrix")
     private val projectorTransformMatrix = projectorShader.findUniformMat4("ProjectorTransformMatrix")
-    private val projectorInverseTransformMatrix = projectorShader.findUniformMat4("ProjectorInverseTransformMatrix")
-
-    private val invProjMat = projectorShader.findUniformMat4("InvProjMat")
 
     private val cameraPosition = projectorShader.findUniform3f("CameraPosition")
     private val projectorPosition = projectorShader.findUniform3f("ProjectorPosition")
 
     private val viewPort = projectorShader.findUniform4i("ViewPort")
 
-    private val nearPlane = projectorShader.findUniform1f("NearPlane")
-    private val farPlane = projectorShader.findUniform1f("NearPlane")
-
 
     private val outMat = Matrix4f()
     override fun onWorldRendered(matrices: MatrixStack, camera: Camera, tickDelta: Float, nanoTime: Long) {
         val client = MinecraftClient.getInstance()
         viewPort.set(0, 0, client.window.framebufferWidth + 0, client.window.framebufferHeight + 0)
-        nearPlane.set(0.1f)
-        farPlane.set(1000.0f)
         if (FabricVisionClient.isRenderingProjector) {
             projectorPosition.set(camera.pos.x.toFloat(), camera.pos.y.toFloat(), camera.pos.z.toFloat())
-            projectorInverseTransformMatrix.set(GlMatrices.getInverseTransformMatrix(outMat))
-            projectorTransformMatrix.set(outMat.invert())
+            projectorTransformMatrix.set(GlMatrices.getInverseTransformMatrix(outMat).invert())
         } else {
             cameraPosition.set(camera.pos.x.toFloat(), camera.pos.y.toFloat(), camera.pos.z.toFloat())
             mainInverseTransformMatrix.set(GlMatrices.getInverseTransformMatrix(outMat))
-            mainTransformMatrix.set(outMat.invert())
         }
     }
 
     override fun renderShaderEffects(tickDelta: Float) {
         projectorShader.render(tickDelta)
     }
-
-    private val Camera.viewMatrix: Matrix4f
-        get() {
-            val viewMatrix = Matrix4f()
-
-            val direction = Vector3f(
-                -cos(Math.toRadians(yaw.toDouble())).toFloat() * cos(Math.toRadians(pitch.toDouble())).toFloat(),
-                -sin(Math.toRadians(pitch.toDouble())).toFloat(),
-                sin(Math.toRadians(yaw.toDouble())).toFloat() * cos(Math.toRadians(pitch.toDouble())).toFloat()
-            ).normalize()
-
-            // Calculate camera right and up vectors
-            val right = Vector3f(1.0f, 0.0f, 0.0f).cross(direction).normalize()
-            val up = direction.cross(right)
-
-            // Set the view matrix using JOML
-            viewMatrix.set(
-                right.x, up.x, -direction.x, 0.0f,
-                right.y, up.y, -direction.y, 0.0f,
-                right.z, up.z, -direction.z, 0.0f,
-                -right.dot(pos.x.toFloat(), pos.y.toFloat(), pos.z.toFloat()),
-                -up.dot(pos.x.toFloat(), pos.y.toFloat(), pos.z.toFloat()),
-                direction.dot(pos.x.toFloat(), pos.y.toFloat(), pos.z.toFloat()),
-                1.0f
-            )
-
-            return viewMatrix
-        }
 
 
 }
--- src/main/resources/assets/fabricvision/shaders/program/projector.fsh
+++ src/main/resources/assets/fabricvision/shaders/program/projector.fsh
@@ -1,26 +1,16 @@
 #version 150
 
-in vec2 texCoord0;
-in vec4 vertexPos;
-
 uniform sampler2D MainSampler;
 uniform sampler2D MainDepthSampler;
 
 uniform sampler2D ProjectorSampler;
 uniform sampler2D ProjectorDepthSampler;
 
-uniform mat4 MainTransformMatrix;
 uniform mat4 MainInverseTransformMatrix;
 uniform mat4 ProjectorTransformMatrix;
-uniform mat4 ProjectorInverseTransformMatrix;
-
-uniform mat4 InvProjMat;
 
 uniform vec3 CameraPosition;
 uniform vec3 ProjectorPosition;
-
-uniform float NearPlane;
-uniform float FarPlane;
 
 uniform ivec4 ViewPort;
 
@@ -31,10 +21,7 @@ void main() {
 	float mainDepth = texelFetch(MainDepthSampler, ivec2(gl_FragCoord.xy), 0).r;
 
 	vec3 windowPos = vec3(gl_FragCoord.xy, mainDepth);
-	vec3 ndc = vec3(
-		windowPos.xy / ViewPort.zw * 2.0 - 1.0,
-		windowPos.z * 2.0 - 1.0
-	);
+	vec3 ndc = vec3(windowPos.xy / ViewPort.zw * 2.0 - 1.0, windowPos.z * 2.0 - 1.0);
 	vec4 world0 = MainInverseTransformMatrix * vec4(ndc, 1.0);
 	vec3 world = world0.xyz / world0.w;
 
@@ -47,11 +34,7 @@ void main() {
 	vec4 projectorTexture = texture(ProjectorSampler, vec2(projectorTexCoords.x, 1.0 - projectorTexCoords.y));
 	float projectorTextureDepth = texture(ProjectorDepthSampler, projectorTexCoords).r;
 
-	if(
-		all(lessThanEqual(abs(ndcProjector), vec3(1.0))) &&
-		projectorTexture.a > 0.0 &&
-		ndcProjector.z * 0.5 + 0.5 - 0.000001 /* to avoid z-fighting */ <= projectorTextureDepth
-	) {
+	if(all(lessThanEqual(abs(ndcProjector), vec3(1.0))) && projectorTexture.a > 0.0 && ndcProjector.z * 0.5 + 0.5 - 0.00001 <= projectorTextureDepth) {
 		fragColor = mix(projectorTexture, mainTexture, 0.5);
 	} else {
 		fragColor = mainTexture;
